c31e803d8b4662488d8723f1081f58380bbc844e
Cheledinov Igor <cheledinov@gmail.com>
2014-02-09 22:24:37 +0300

    Small changes.

--- mysqlbackup.py
+++ mysqlbackup.py
@@ -2,6 +2,9 @@
 
 __author__ = 'chilic'
 
+# @todo Add ignore tables.
+# @todo Add logging.
+
 import ConfigParser
 import os
 from datetime import date
@@ -10,11 +13,14 @@ from calendar import monthrange
 # On Debian, /etc/mysql/debian.cnf contains 'root' a like login and password.
 from shutil import copyfile
 
+# Configuration.
 config = ConfigParser.ConfigParser()
 config.read("/etc/mysql/debian.cnf")
+
 username = config.get('client', 'user')
 password = config.get('client', 'password')
 hostname = config.get('client', 'host')
+
 backup_dir = "/backup/mysql"  # Root for backups.
 backup_dir_daily = "%s/daily" % backup_dir
 backup_dir_weekly = "%s/weekly" % backup_dir
@@ -23,6 +29,9 @@ weekday_for_first_day, last_month_day = monthrange(date.today().year, date.today
 
 # Mysqldump options. @see http://dev.mysql.com/doc/refman/5.1/en/mysqldump.html
 options = "-f --skip-lock-tables"
+
+# Enable log.
+log = True
 
 if not os.path.exists(backup_dir_daily):
     os.makedirs(backup_dir_daily)
@@ -34,7 +43,7 @@ if not os.path.exists(backup_dir_monthly):
     os.makedirs(backup_dir_monthly)
 
 # Get a list of databases with :
-database_list_command = "mysql -u %s -p%s -h %s --silent -N -e 'show databases'" % (username, password, hostname)
+database_list_command = "mysql -u%s -p%s -h%s --silent -N -e 'show databases'" % (username, password, hostname)
 for database in os.popen(database_list_command).readlines():
     database = database.strip()
     if database == 'information_schema':
@@ -43,7 +52,7 @@ for database in os.popen(database_list_command).readlines():
         continue
 
     filename = "%s/%s.sql" % (backup_dir_daily, database)
-    proc = os.popen("mysqldump -u %s -p%s -h%s %s %s | gzip -c > %s.gz" % (
+    proc = os.popen("mysqldump -u%s -p%s -h%s %s %s | gzip -c > %s.gz" % (
         username, password, hostname, options, database, filename))
     proc.close()
 
